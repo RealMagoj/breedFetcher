71b2fe8df61377c91f20f3289f55ae985a2f2e29
Dom Golac <domgolac@gmail.com>
2021-05-01 07:09:40 +0000

    create breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,20 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, response, body) => {
-  if (err || response.statusCode !== 200) {
-    console.log('There was an error.');
-  } else {
-    const data = JSON.parse(body);
-    if (data.length < 1) {
-      console.log('Breed not found.');
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+    if (err || response.statusCode !== 200) {
+      return callback(err || 'ERROR', null);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length < 1) {
+        return callback(null, 'Breed not found.');
+      } else {
+        return callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
